199d7c3f63775f701ea622d81162d2dcbff74933
im23b-busere <busere@bzz.ch>
2025-04-14 13:05:16 +0200

    async finished

---
+++ 2/async.js
@@ -0,0 +1,12 @@
+async function simuliereVerzoegerung(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function addiereNachVerzoegerung(a, b, ms) {
+    await simuliereVerzoegerung(ms);
+    console.log(a + b);
+}
+
+addiereNachVerzoegerung(3, 7, 2000);
